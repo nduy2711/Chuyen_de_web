03e41d323b01fb7918bf3c3589159ac1a9942a43
nduy2711 <nhatduy2711.govap@gmail.com>
2025-02-13 14:09:21 +0700

    view file

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
